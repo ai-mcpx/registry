861728619b29f4db8ad8d103b6e061b07fa49083
Jia Jia <angersax@sina.com>
2025-08-04 16:21:20 +0800

    feat(swagger): add support for swagger ui

--- internal/api/handlers/v0/swagger.go
+++ internal/api/handlers/v0/swagger.go
@@ -2,12 +2,15 @@
 package v0
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	_ "github.com/swaggo/files" // Swagger files needed for embedding
 	httpSwagger "github.com/swaggo/http-swagger"
+	"gopkg.in/yaml.v3"
 )
 
 // SwaggerHandler returns a handler that serves the Swagger UI
@@ -19,9 +22,19 @@ func SwaggerHandler() http.HandlerFunc {
 			return
 		}
 
+		// Determine the JSON URL based on how we're being accessed
+		var jsonURL string
+		if r.Header.Get("X-Forwarded-Proto") != "" || r.Header.Get("X-Real-IP") != "" {
+			// Being accessed through a proxy (nginx), use external URL
+			jsonURL = "/api/swagger/doc.json"
+		} else {
+			// Direct access, use internal URL
+			jsonURL = "/v0/swagger/doc.json"
+		}
+
 		// Serve the Swagger UI
 		handler := httpSwagger.Handler(
-			httpSwagger.URL("/v0/swagger/doc.json"), // The URL to the generated Swagger JSON
+			httpSwagger.URL(jsonURL), // Use the appropriate URL for the JSON
 			httpSwagger.DeepLinking(true),
 		)
 
@@ -43,7 +56,38 @@ func SwaggerJSONHandler() http.HandlerFunc {
 		// Path to the swagger YAML file
 		swaggerFilePath := filepath.Join(workDir, "internal", "docs", "swagger.yaml")
 
-		// Serve the file directly with the correct content type
-		http.ServeFile(w, r, swaggerFilePath)
+		// Read the YAML file
+		file, err := os.Open(swaggerFilePath)
+		if err != nil {
+			http.Error(w, "Unable to open swagger file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		// Read the YAML content
+		yamlData, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Unable to read swagger file", http.StatusInternalServerError)
+			return
+		}
+
+		// Parse YAML
+		var swaggerSpec interface{}
+		err = yaml.Unmarshal(yamlData, &swaggerSpec)
+		if err != nil {
+			http.Error(w, "Unable to parse swagger YAML", http.StatusInternalServerError)
+			return
+		}
+
+		// Convert to JSON
+		jsonData, err := json.Marshal(swaggerSpec)
+		if err != nil {
+			http.Error(w, "Unable to convert swagger to JSON", http.StatusInternalServerError)
+			return
+		}
+
+		// Set content type and serve JSON
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
 	}
 }
